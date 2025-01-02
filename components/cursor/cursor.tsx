664f837c3f3d6d7bdb0a058da136c156910e4ab3
niemand8080 <benhamster443@gmail.com>
2025-01-02 17:38:11 +0100

    update

--- components/cursor/cursor.tsx
+++ components/cursor/cursor.tsx
@@ -20,7 +20,7 @@ const CursorContext = createContext<CursorContextProps | undefined>(undefined);
 export const CursorProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const [color, setColor] = useState<string>('hsl(var(--foreground))');
   const [activeColor, setActiveColor] = useState<string>('hsl(var(--primary))');
-  const [variant, setVariant] = useState<CursorVariant>("default");
+  const [variant, setVariant] = useState<CursorVariant>("glass");
   const [invert, setInvert] = useState<boolean>(true);
 
   return (
@@ -58,7 +58,7 @@ export const Cursor = () => {
   const cursorRef = useRef<HTMLDivElement>(null);
   const [state, setState] = useState<CursorState>('default');
   const [isActive, setIsActive] = useState<boolean>(false);
-  const [fontSize, setFontSize] = useState<string>("16px");
+  const [fontSize, setFontSize] = useState<number>(16);
 
   useEffect(() => {
     const handleMouseMove = (e: MouseEvent) => {
@@ -71,12 +71,18 @@ export const Cursor = () => {
       
       // State
       const element = document.elementFromPoint(clientX, clientY) as HTMLElement;
+      if (element == null) return;
       const classList = element.classList.value.split(' ');
       const style = window.getComputedStyle(element);
       const name = element.tagName;
-      setFontSize(style.fontSize);
+      const fontSize = Number(style.fontSize.replace('px', ''));
+      setFontSize(fontSize);
 
-      let button = classList.includes('cursor-pointer') || name == "BUTTON" || element.parentElement?.tagName == 'BUTTON' || element.parentElement?.tagName == "svg" && element.parentElement?.parentElement?.tagName == "BUTTON";
+      let button = 
+        classList.includes('cursor-pointer') || 
+        name == "BUTTON" || 
+        element.parentElement?.tagName == 'BUTTON' || 
+        element.parentElement?.tagName == "svg" && element.parentElement?.parentElement?.tagName == "BUTTON";
       let text = classList.includes('cursor-text') || name == "SPAN" || name == "P";
       
       if (button) setState('pointer');
@@ -84,11 +90,11 @@ export const Cursor = () => {
       else setState('default');
     }
 
-    const handleMouseDown = () => {
+    const handleMouseDown = (e: MouseEvent) => {
       setIsActive(true);
     }
     
-    const handleMouseUp = () => {
+    const handleMouseUp = (e: MouseEvent) => {
       setIsActive(false);
     }
 
@@ -104,26 +110,24 @@ export const Cursor = () => {
   }, [isActive]);
 
   return (
-    <>
-      <div
-        ref={cursorRef}
-        aria-hidden="true"
-        className={cn(
-          "size-6 rounded-full fixed pointer-events-none backdrop-invert-0",
-          isActive && "size-5",
-          state == 'text' && `w-1 h-8 mx-[0.7rem] -translate-y-[40%]`,
-          state == 'pointer' && "border",
-          variant == 'glass' && "opacity-20 backdrop-blur-sm",
-          invert && "backdrop-invert"
-        )}
-        style={{
-          height: `${state == 'text' ? fontSize : ''}`,
-          transition: 'all 0.15s, top 0s, left 0s', 
-          top: "-100px",
-          left: "-100px",
-          background: `${state == 'pointer' || state == 'text' ? activeColor : color}`,
-        }}
-      ></div>
-    </>
+    <div
+      ref={cursorRef}
+      aria-hidden="true"
+      className={cn(
+        "size-6 rounded-full fixed pointer-events-none",
+        isActive && "size-5 mt-0.5 ml-0.5",
+        state == 'text' && `w-1 h-8 mx-[0.7rem] -translate-y-[40%]`,
+        state == 'pointer' && "border",
+        variant == 'glass' && "opacity-20 backdrop-blur-sm",
+        invert && "backdrop-invert opacity-20"
+      )}
+      style={{
+        height: `${state == 'text' ? (isActive ? fontSize - ((fontSize / 100) * 10) : fontSize) + "px" : ''}`,
+        transition: 'all 0.15s, top 0s, left 0s', 
+        top: "-100px",
+        left: "-100px",
+        background: `${state == 'pointer' || state == 'text' ? activeColor : color}`,
+      }}
+    ></div>
   )
 }
